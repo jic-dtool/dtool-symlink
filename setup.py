cf26ce99243d0545ee9699a3afb00209727101af
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-09-11 11:59:54 +0100

    Add entry point for the SymLinkStorageBroker

--- setup.py
+++ setup.py
@@ -15,6 +15,11 @@ setup(
     author_email="tjelvar.olsson@jic.ac.uk",
     url=url,
     install_requires=[],
+    entry_points={
+        "dtool.storage_brokers": [
+            "SymLinkStorageBroker=dtool_symlink.storagebroker:SymLinkStorageBroker",
+        ],
+    },
     download_url="{}/tarball/{}".format(url, version),
     license="MIT"
 )
