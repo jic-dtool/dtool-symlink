b7eff1cc716ed05c9efdc7ed56bcf35781e81701
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2018-02-09 10:55:25 +0000

    Simplify API and add error checking / validation

--- dtool_symlink/storagebroker.py
+++ dtool_symlink/storagebroker.py
@@ -1,44 +1,8 @@
 """Symbolic link storage broker module."""
 
-import sys
 import os
 
-import click
-
-try:
-    import Tkinter as tk
-except ImportError:
-    try:
-        import tkinter as tk
-    except ImportError:
-        pass
-
-try:
-    import tkFileDialog
-except ImportError:
-    pass
-
 from dtoolcore.storagebroker import DiskStorageBroker
-
-
-def _get_data_directory():
-    try:
-        root = tk.Tk()
-        root.withdraw()
-        data_directory = tkFileDialog.askdirectory()
-    except:
-        data_directory = click.prompt(
-            "Please enter the path to the data directory",
-            type=click.Path(
-                exists=True,
-                file_okay=False,
-                resolve_path=True
-            )
-        )
-    if data_directory is "":
-        sys.exit()
-
-    return data_directory
 
 
 class SymLinkStorageBroker(DiskStorageBroker):
@@ -53,10 +17,17 @@ class SymLinkStorageBroker(DiskStorageBroker):
             self._overlays_abspath
         ]
 
-    def create_structure(self, get_data_dir_func=_get_data_directory):
+    def create_structure(self):
         super(SymLinkStorageBroker, self).create_structure()
-        data_directory = get_data_dir_func()
-        os.symlink(data_directory, self._data_abspath)
+        try:
+            if not os.path.isdir(self.symlink_path):
+                message = "No such directory: '{}'".format(self.symlink_path)  # NOQA
+                raise(IOError(message))
+
+        except AttributeError:
+            message = "The 'symlink_path' attribute needs to be added to the 'SymLinkStorageBroker' instance before calling the 'create_structure' method"  # NOQA
+            raise(AttributeError(message))
+        os.symlink(self.symlink_path, self._data_abspath)
 
     def put_item(self, fpath, relpath):
         raise(NotImplementedError())
--- tests/test_SymLinkStorageBroker.py
+++ tests/test_SymLinkStorageBroker.py
@@ -12,20 +12,18 @@ def test_SymLinkStroageBoker_functional(tmp_dir_fixture):  # NOQA
 
     from dtool_symlink.storagebroker import SymLinkStorageBroker
 
-    def get_data_dir():
-        return TEST_SAMPLE_DATA
-
     uri = os.path.join(tmp_dir_fixture, "test_storage_broker")
 
     symlink_storage_broker = SymLinkStorageBroker(
         uri=uri,
         config_path=None)
+    symlink_storage_broker.symlink_path = TEST_SAMPLE_DATA
 
     # Ensure it is not possible to overwrite data in it.
     with pytest.raises(NotImplementedError):
         symlink_storage_broker.put_item("dummy.txt", "dummy.txt")
 
-    symlink_storage_broker.create_structure(get_data_dir_func=get_data_dir)
+    symlink_storage_broker.create_structure()
 
     assert os.path.islink(symlink_storage_broker._data_abspath)
 
@@ -34,9 +32,6 @@ def test_SymLinkStorageBroker_complete_cycle(tmp_dir_fixture):  # NOQA
 
     from dtool_symlink.storagebroker import SymLinkStorageBroker
     import dtoolcore
-
-    def get_data_dir():
-        return TEST_SAMPLE_DATA
 
     name = "test_symlink_storage_broker"
     admin_metadata = dtoolcore.generate_admin_metadata(name)
@@ -47,7 +42,8 @@ def test_SymLinkStorageBroker_complete_cycle(tmp_dir_fixture):  # NOQA
         uri=uri,
         config_path=None
     )
-    symlink_storage_broker.create_structure(get_data_dir_func=get_data_dir)
+    symlink_storage_broker.symlink_path = TEST_SAMPLE_DATA
+    symlink_storage_broker.create_structure()
 
     proto_dataset = dtoolcore.ProtoDataSet(uri, admin_metadata, None)
     proto_dataset._storage_broker = symlink_storage_broker
@@ -56,3 +52,38 @@ def test_SymLinkStorageBroker_complete_cycle(tmp_dir_fixture):  # NOQA
 
     dataset = dtoolcore.DataSet.from_uri(uri)
     assert len(dataset.identifiers) == 7
+
+
+def test_SymLinkStroageBoker_not_setting_symlink_path_attribute(tmp_dir_fixture):  # NOQA
+
+    from dtool_symlink.storagebroker import SymLinkStorageBroker
+
+    uri = os.path.join(tmp_dir_fixture, "test_storage_broker")
+
+    symlink_storage_broker = SymLinkStorageBroker(
+        uri=uri,
+        config_path=None)
+
+    with pytest.raises(AttributeError) as excinfo:
+        symlink_storage_broker.create_structure()
+
+    message = "The 'symlink_path' attribute needs to be added to the 'SymLinkStorageBroker' instance before calling the 'create_structure' method"  # NOQA
+    assert excinfo.value.args[0] == message
+
+def test_SymLinkStroageBoker_with_invalide_symlink_path(tmp_dir_fixture):  # NOQA
+
+    from dtool_symlink.storagebroker import SymLinkStorageBroker
+
+    uri = os.path.join(tmp_dir_fixture, "test_storage_broker")
+
+    symlink_storage_broker = SymLinkStorageBroker(
+        uri=uri,
+        config_path=None)
+
+    symlink_storage_broker.symlink_path = "/dont/exist"
+
+    with pytest.raises(IOError) as excinfo:
+        symlink_storage_broker.create_structure()
+
+    message = "No such directory: '/dont/exist'"  # NOQA
+    assert excinfo.value.args[0] == message
